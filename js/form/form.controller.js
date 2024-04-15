486d6da015c148ce4178aaf9cc958ade84faf4fc
perov_dev <mihailperov90@yandex.ru>
2024-04-15 18:06:27 +0300

    MVC refactoring + local storage data flow done

--- js/form/form.controller.js
+++ js/form/form.controller.js
@@ -1,42 +1,24 @@
 import * as formView from './form.view.js';
-import { insertTestData, testData } from './form.test-data.js';
-import { requestModel } from '../model.js';
-
-/*
-1. взять из формы значения
-2. сформировать объект
-3. сохранить его в локал сторадж
-*/
+import * as test from './form.test-data.js';
+import * as model from '../model.js';
 
 const form = formView.elements
 
 formView.elements.form.addEventListener('submit', (e) => {
     e.preventDefault()
-
-    const date = new Date()
-    const formatedDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
    
     const newCostomer = {
-        id: requestModel.length,
         name: form.formName.value,
         phone: form.formPhone.value,
         email: form.formEmail.value,
-        product: form.formProduct.value,
-        date: formatedDate,
-        status: 'Новая'
-    }
-
-    if (localStorage.getItem('request') === null) {
-        const request = []
-        request.push(newCostomer)
-        localStorage.setItem('request', JSON.stringify(request))
-    } else if (localStorage.getItem('request') !== null) {
-        let dataFromLS = JSON.parse(localStorage.getItem('request'))
-        dataFromLS.push(newCostomer)
-        localStorage.setItem('request', JSON.stringify(dataFromLS))
+        product: form.formProduct.value
     }
     
-    insertTestData(testData)
+    let formatedCostomer = model.createNewCostomer(newCostomer)
+
+    model.loadNewDataToLS(formatedCostomer)
+    
+    test.insertTestData(test.testData)
 })
 
 
--- js/model.js
+++ js/model.js
@@ -1,6 +1,34 @@
-const dataFromLS = JSON.parse(localStorage.getItem('request'))
-let requestModel = []
+let requestModel
 
-if (dataFromLS) requestModel = [...dataFromLS]
+const setInitArrayToLS = () => localStorage.setItem('request', JSON.stringify([]))
 
-export { requestModel }
+const loadFromLS = () => JSON.parse(localStorage.getItem('request')) ? JSON.parse(localStorage.getItem('request')) : undefined
+
+const loadNewDataToLS = (newData) => {
+    const dataFromLS = JSON.parse(localStorage.getItem('request'))
+    const newDataToLS = [...dataFromLS, newData]
+    localStorage.setItem('request', JSON.stringify(newDataToLS))
+}
+
+const createNewCostomer = (costomerFromForm) => {
+    const date = new Date()
+    const formatedDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
+    const id = Math.floor(Math.random() * 99999)
+
+    return {
+        id,
+        name: costomerFromForm.name,
+        phone: costomerFromForm.phone,
+        email: costomerFromForm.email,
+        product: costomerFromForm.product,
+        date: formatedDate,
+        status: 'Новая'
+    }
+}
+
+if (!localStorage.getItem('request')) setInitArrayToLS()
+
+requestModel = loadFromLS()
+
+export { requestModel, loadNewDataToLS, createNewCostomer, loadFromLS }
+
